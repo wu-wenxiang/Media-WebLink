6fc6662dddfdd5182182566982dcadbd87b1699f
Wenxiang Wu <maodouzi@gmail.com>
2017-05-18 17:56:57 +0800

    add qiniu

---
+++ download.py
@@ -0,0 +1,392 @@
+aStr ='''
+0efa7864c26b41f1bf86baa98eec97a8-ProcExp-MemoryLeak.jpg	
+image/jpeg	229.78 KB	2017-04-26 13:30:07	  
+	
+0efa7864c26b41f1bf86baa98eec97a8-ServicePWD.jpg	
+image/jpeg	111.43 KB	2017-04-26 11:57:06	  
+	
+0f411cda89af489da677eaec9d2e3a5f-SafeModeNetworkTest.docx	
+application/vnd.openxmlformats-officedocument.wordprocessingml.document	651.97 KB	2016-06-29 20:35:36	  
+	
+1426a7be556245a0b68defe10ea6ee5a-Africa.jpg	
+image/jpeg	598.33 KB	2016-11-22 12:02:09	  
+	
+1426a7be556245a0b68defe10ea6ee5a-dengshan.jpg	
+image/jpeg	351.78 KB	2016-06-03 08:19:23	  
+	
+1426a7be556245a0b68defe10ea6ee5a-gonglu.jpeg	
+image/jpeg	311.37 KB	2016-06-03 08:19:16	  
+	
+1426a7be556245a0b68defe10ea6ee5a-jieti.jpeg	
+image/jpeg	423.03 KB	2016-06-03 08:19:25	  
+	
+1426a7be556245a0b68defe10ea6ee5a-sky.jpg	
+image/jpeg	32.66 KB	2016-11-22 11:54:19	  
+	
+1689ecbf60a940cc80452289d2d5df7d-RedX.png	
+image/png	14.53 KB	2016-06-20 15:51:04	  
+	
+171e26bd0d104c00857f967cbeb77fbc-JSFramworkList.png	
+image/png	191.10 KB	2016-05-22 20:35:01	  
+	
+171e26bd0d104c00857f967cbeb77fbc-MeteorFramework.png	
+image/png	579.89 KB	2016-05-22 20:40:23	  
+	
+171e26bd0d104c00857f967cbeb77fbc-WebFrontendBackendModel.png	
+image/png	136.35 KB	2016-05-22 21:07:17	  
+	
+1faa032c59274913b7473091b5c42fa7-AssertException.png	
+image/png	3.60 KB	2016-06-13 20:40:09	  
+	
+1faa032c59274913b7473091b5c42fa7-Deprecated.png	
+image/png	5.88 KB	2016-06-13 20:24:43	  
+	
+1faa032c59274913b7473091b5c42fa7-NormalAssert.png	
+image/png	6.58 KB	2016-06-13 20:40:09	  
+	
+1faa032c59274913b7473091b5c42fa7-SpecificAssert.png	
+image/png	8.31 KB	2016-06-13 20:40:09	  
+	
+1faa032c59274913b7473091b5c42fa7-SpecificEqual.png	
+image/png	4.36 KB	2016-06-13 20:40:09	  
+	
+37369909dc8d43308cb7d298bf998297-internals-arch.gif	
+image/gif	4.54 KB	2017-04-28 19:03:31	  
+	
+37369909dc8d43308cb7d298bf998297-winpcap.png	
+image/png	51.71 KB	2017-04-28 19:03:31	  
+	
+4d7ac251b31c453382216147ee032bd5-AddModel1.png	
+image/png	15.25 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-AddModel2.png	
+image/png	25.59 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-AddModel3.png	
+image/png	41.34 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-AddModel4.png	
+image/png	15.76 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-AddModel5.png	
+image/png	5.50 KB	2016-06-03 14:32:03	  
+	
+4d7ac251b31c453382216147ee032bd5-AddModel6.png	
+image/png	12.21 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-AddMovieController1.png	
+image/png	11.78 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-AddMovieController2.png	
+image/png	11.74 KB	2016-06-03 16:26:40	  
+	
+4d7ac251b31c453382216147ee032bd5-AddMovieController3.png	
+image/png	7.17 KB	2016-06-03 16:26:40	  
+	
+4d7ac251b31c453382216147ee032bd5-AddView1.png	
+image/png	10.04 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-AddView2.png	
+image/png	9.09 KB	2016-06-03 14:32:03	  
+	
+4d7ac251b31c453382216147ee032bd5-HTMLEncode1.png	
+image/png	17.96 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-HTMLEncode2.png	
+image/png	17.67 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-HTMLEncode3.png	
+image/png	16.02 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-QueryString.png	
+image/png	27.58 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-QueryStringDefault.png	
+image/png	6.98 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-RequestValidationError.png	
+image/png	31.32 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-SetPermission1.png	
+image/png	45.81 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-SetPermission2.png	
+image/png	22.62 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-ShowAppData1.png	
+image/png	9.28 KB	2016-06-03 16:26:42	  
+	
+4d7ac251b31c453382216147ee032bd5-ShowAppData2.png	
+image/png	7.78 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-ShowMovieEditPage.png	
+image/png	23.79 KB	2016-06-03 16:26:42	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext1.png	
+image/png	16.22 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext2.png	
+image/png	7.11 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext3.png	
+image/png	13.49 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext4.png	
+image/png	4.90 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext5.png	
+image/png	13.61 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext6.png	
+image/png	10.34 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext7.png	
+image/png	12.92 KB	2016-06-03 16:26:41	  
+	
+4d7ac251b31c453382216147ee032bd5-VSDBContext8.png	
+image/png	18.69 KB	2016-06-03 16:26:42	  
+	
+4d7ac251b31c453382216147ee032bd5-ViewLayout1.png	
+image/png	50.54 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-ViewLayout2.png	
+image/png	13.85 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-addController.png	
+image/png	39.44 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-addControllerMVC5.png	
+image/png	28.02 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-addControllerName.png	
+image/png	2.91 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-cshtml1.png	
+image/png	21.32 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-cshtml2.png	
+image/png	13.46 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-cshtml3.png	
+image/png	7.52 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-cshtml4.png	
+image/png	76.69 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-cshtml5.png	
+image/png	27.65 KB	2016-06-03 14:32:04	  
+	
+4d7ac251b31c453382216147ee032bd5-iisExpressSetting.png	
+image/png	17.58 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-iisExpressShowAll.png	
+image/png	11.59 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-newProject.png	
+image/png	39.03 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-newProjectMVC.png	
+image/png	35.19 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-newProjectWeb.png	
+image/png	28.21 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-routerFormat.png	
+image/png	33.02 KB	2016-05-21 22:50:39	  
+	
+4d7ac251b31c453382216147ee032bd5-routerGlobal.png	
+image/png	30.35 KB	2016-05-21 22:50:40	  
+	
+4d8fa0ac19d8424ca40c1035bd057cc4-F12-Network.png	
+image/png	27.79 KB	2016-07-19 20:20:54	  
+	
+4d8fa0ac19d8424ca40c1035bd057cc4-WrongEncode.png	
+image/png	159.71 KB	2016-06-20 17:44:00	  
+	
+73b815e99c5f428a87de015e7d2e3817-testSNICert.png	
+image/png	25.14 KB	2016-08-23 17:11:58	  
+	
+7a595dbd6d8c42a782899ab594ac35e6-SQL-Netbios.png	
+image/png	9.92 KB	2016-09-23 18:59:03	  
+	
+92fbb8d6f85246c8817962c5af6272ca-Download-Failure-OpenDirectly.png	
+image/png	43.28 KB	2017-05-03 10:32:37	  
+	
+92fbb8d6f85246c8817962c5af6272ca-Download-OpenDirectly.png	
+image/png	231.61 KB	2017-05-03 10:32:38	  
+	
+92fbb8d6f85246c8817962c5af6272ca-Download-SuccessOpenDirectly.png	
+image/png	83.98 KB	2017-05-03 10:32:37	  
+	
+9b3edb2268af4e48b7bef08b15b0ff74-localhostFTP.png	
+image/png	18.33 KB	2016-08-30 09:47:11	  
+	
+9e7c39ba1fa54c17b394a1918e4a0f3d-Netmon-Layer.jpg	
+image/jpeg	11.86 KB	2016-06-30 13:05:43	  
+	
+9e7c39ba1fa54c17b394a1918e4a0f3d-NetworkSettings.jpg	
+image/jpeg	52.35 KB	2016-09-23 18:14:59	  
+	
+9e7c39ba1fa54c17b394a1918e4a0f3d-Windows-OSI.jpg	
+image/jpeg	29.93 KB	2016-06-30 13:05:43	  
+	
+Common-9fa9e352b59943ecaeee659bec62c519-qrcode_for_gh_6d13cd886e31_258.jpg	
+image/jpeg	26.29 KB	2016-06-20 21:50:12	  
+	
+Test-f4f338218d4245cf8fad114381f0e30a-CloudTools.png	
+image/png	65.35 KB	2016-07-28 10:45:42	  
+	
+Test-f4f338218d4245cf8fad114381f0e30a-sport.mp4	
+video/mp4	2.76 MB	2016-07-28 09:22:58	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-Chrome-DevTools.jpeg	
+image/jpeg	155.88 KB	2017-03-28 14:58:52	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-Everybody-hate-IE6.jpg	
+image/jpeg	84.08 KB	2017-03-28 10:56:20	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Addon.jpg	
+image/jpeg	81.05 KB	2017-03-28 11:32:17	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Bank.jpg	
+image/jpeg	34.40 KB	2017-03-28 11:36:39	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Craft.png	
+image/png	117.69 KB	2016-05-23 13:54:44	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-EPM.png	
+image/png	35.59 KB	2016-05-23 16:41:24	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-FullScreen.png	
+image/png	777.21 KB	2016-05-23 16:09:53	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Internal.png	
+image/png	59.78 KB	2017-03-28 14:58:51	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Market-Share.png	
+image/png	32.43 KB	2016-05-23 12:25:53	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Open-Slow.jpg	
+image/jpeg	48.18 KB	2017-03-28 10:56:20	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Proxy.png	
+image/png	47.65 KB	2017-03-28 14:58:51	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Security.jpg	
+image/jpeg	40.08 KB	2017-03-28 11:32:16	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-Support-Lifecycle.png	
+image/png	151.33 KB	2016-05-23 15:34:09	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-do-not-default-browser.jpg	
+image/jpeg	74.81 KB	2017-03-28 10:56:18	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-do-not-fight.jpg	
+image/jpeg	68.58 KB	2017-03-28 10:56:18	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-is-old.jpg	
+image/jpeg	76.63 KB	2017-03-28 10:56:20	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-IE-slow.jpg	
+image/jpeg	99.48 KB	2017-03-28 10:56:20	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-MS-Security-Bulletin.png	
+image/png	128.52 KB	2017-02-09 08:46:35	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-Mads-C#.png	
+image/png	827.75 KB	2016-05-23 15:48:52	  
+	
+b257bb1d42524fbc8013ab5c945d1fc3-iexplore-extoff.jpg	
+image/jpeg	23.27 KB	2017-03-30 07:13:59	  
+	
+b3f0f5669f8c42149d004c1bad126796-SideBySide-Cache.jpg	
+image/jpeg	36.97 KB	2016-08-13 18:28:03	  
+	
+bc2b08b4a56d41f5aaa545c63edde7c7-CMD-Try-Finally-CloseProgram.png	
+image/png	20.54 KB	2016-08-17 22:50:23	  
+	
+bc2b08b4a56d41f5aaa545c63edde7c7-CMD-Try-Finally.png	
+image/png	77.78 KB	2016-08-17 22:50:23	  
+	
+bc2b08b4a56d41f5aaa545c63edde7c7-VS-Try-Finally.png	
+image/png	136.11 KB	2016-08-17 22:50:23	  
+	
+c357331791434ffe98d84126ee441091-Add-UNC-To-LocalIntranet.png	
+image/png	44.52 KB	2016-07-05 14:22:46	  
+	
+c357331791434ffe98d84126ee441091-DiffBin.png	
+image/png	38.13 KB	2016-07-27 15:22:07	  
+	
+c357331791434ffe98d84126ee441091-Low-Virtual-Memory.jpg	
+image/jpeg	42.19 KB	2016-08-02 08:10:31	  
+	
+c357331791434ffe98d84126ee441091-Set-PageFile.jpg	
+image/jpeg	23.41 KB	2016-08-02 08:06:09	  
+	
+c357331791434ffe98d84126ee441091-VS-Dev-Cmd-Prompt.png	
+image/png	19.91 KB	2016-07-27 14:25:42	  
+	
+c357331791434ffe98d84126ee441091-VS_NXCompat.png	
+image/png	88.52 KB	2016-07-27 15:29:13	  
+	
+c357331791434ffe98d84126ee441091-environmenVariable.png	
+image/png	39.61 KB	2016-06-18 20:29:12	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-App-Controller.png	
+image/png	397.23 KB	2016-06-21 20:15:32	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-Auth-OK.png	
+image/png	158.49 KB	2016-06-21 20:00:45	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-Auth.png	
+image/png	311.18 KB	2016-06-20 22:02:20	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-Realtime-Log-Show.png	
+image/png	248.22 KB	2016-06-24 23:47:41	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-Realtime-Log.png	
+image/png	288.69 KB	2016-06-24 23:40:19	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-Site-Articles.png	
+image/png	374.50 KB	2016-06-21 20:15:26	  
+	
+c5b735854b244f629923cd3c4b2715cc-SAE-Site-Root.png	
+image/png	41.01 KB	2016-06-21 20:15:25	  
+	
+c5b735854b244f629923cd3c4b2715cc-Tuling123.png	
+image/png	210.69 KB	2016-06-20 21:58:26	  
+	
+c5b735854b244f629923cd3c4b2715cc-Wechat-Robot.png	
+image/png	368.20 KB	2016-06-21 19:54:58	  
+	
+c5b735854b244f629923cd3c4b2715cc-Wechat-Sub-Controller.png	
+image/png	308.50 KB	2016-06-21 20:15:10	  
+	
+c5b735854b244f629923cd3c4b2715cc-Wechat-Subscription-Init.PNG	
+image/png	160.21 KB	2016-06-20 22:12:25	  
+	
+d051a67046a746a2bc4281098065790d-TestWCF.png	
+image/png	39.58 KB	2016-08-11 20:52:37	  
+	
+ebae808f8441465a8cc2c68ae985ef60-Matlas-SAM.png	
+image/png	45.17 KB	2016-06-22 22:27:56	  
+	
+f2784ba6c70d45f8a4fb2283a76d90d8-DynamicNavError.png	
+image/png	59.54 KB	2016-07-06 14:58:43	  
+	
+f2784ba6c70d45f8a4fb2283a76d90d8-VBScript-Call-COM-Component.jpg	
+image/jpeg	54.44 KB	2016-07-06 15:02:16
+'''
+
+import re
+import urllib
+
+reCmp = re.compile(r'[a-f0-9]{32}')
+bList = [line.strip() for line in aStr.split('\n') if reCmp.search(line)]
+print(len(bList))
+'''
+for line in bList:
+    url = r'http://7xudfs.com1.z0.glb.clouddn.com/%s' % line
+    print(url)
+    urllib.urlretrieve(url, r'C:\Users\wenw\Desktop\st\%s' % line)
+'''
